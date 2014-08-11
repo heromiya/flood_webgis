42c8d7d579294af9425d04a383d609bb24270b76
heromiya <heromiya@heromiya.net>
2014-08-11 15:21:26 +0800

    Better response through Ajax

--- JadurChar.aspx
+++ JadurChar.aspx
@@ -6,101 +6,42 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
     <script type="text/javascript" src="Scripts/jquery-1.7.min.js"></script>
-    <style type="text/css">
-        .auto-style1 {
-            width: 184px;
-        }
-        .auto-style3 {
-            width: 200px;
-        }
-    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
     <ajaxToolkit:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
     </ajaxToolkit:ToolkitScriptManager>
-    <table style="width:100%">
-        <tr>
-            <td style="vertical-align:top; width:200px; height:500px">
-							<asp:ImageButton id="ImageButton1" runat="server" ImageUrl="tools/zoomfull.gif" BorderStyle="Outset"
-								BorderWidth="1px" ToolTip="Zoom All" BorderColor="White" OnClick="zoomFull_Click"></asp:ImageButton>
-							<aspmap:MapToolButton id="MapToolButton1" runat="server" ImageUrl="tools/zoomin.gif" Map="map" ToolTip="Zoom In"></aspmap:MapToolButton>
-							<aspmap:MapToolButton id="MapToolButton2" runat="server" ImageUrl="tools/zoomout.gif" ToolTip="Zoom Out"
-								Map="map" MapTool="ZoomOut"></aspmap:MapToolButton>
-							<aspmap:MapToolButton id="MapToolButton3" runat="server" ImageUrl="tools/pan.gif" ToolTip="Pan" Map="map" MapTool="Pan"></aspmap:MapToolButton>
-                <table class="auto-style1">
-                    <tr>
-                        <td style="vertical-align:top; " class="auto-style3">
-                           <strong>Layer</strong>
-                        </td>
-                    </tr>
-                     <tr>
-                        <td>
-                            <strong>Background Map</strong><br />
-                            <asp:RadioButton id="RadioButton1" Text="Google Maps" TextAlign="Right" AutoPostBack="True" Checked="True" OnCheckedChanged="RadioButton1_CheckedChanged" GroupName="bgmap" runat="server" /><br>
-                            <asp:RadioButton id="RadioButton2" Text="OpenStreeMap" TextAlign="Right" AutoPostBack="True"  Checked="false" OnCheckedChanged="RadioButton2_CheckedChanged" GroupName="bgmap" runat="server" /><br>
-                            <strong>Inudation</strong><br />
-                            <asp:RadioButtonList ID="innudationList" runat="server" AutoPostBack="True">
-                            </asp:RadioButtonList>
-
-						    <img src="WD.jpg" alt="ADB" style="height: 121px; width: 142px"/>
-
-</td>
-                    </tr>
-                </table>
-                            <br />
-                            <asp:CheckBoxList ID="layerList" runat="server" AutoPostBack="True">
-                            </asp:CheckBoxList>
-            </td>
-						<td style="vertical-align:top; width:600px; height:700px"">
-                                 <aspmap:Map id="map" EnableSession="true" runat="server" Width="600px" 
-                                         Height="700px" BackColor="#E6E6FA" ImageFormat="Gif" SmoothingMode="AntiAlias"
-								         FontQuality="ClearType" MapTool="Pan" onmarkerclick="map_MarkerClick" style="margin-top: 0px"></aspmap:Map>
-					         <asp:Label ID="status" runat="server"></asp:Label>
-            </td>
-            <td style="vertical-align:top; width:200px; height:500px">
-               <aspmap:Legend id="legend" runat="server" Width="150px"  ImageFormat="Png" />
-            </td>
-        </tr>
-    </table>
-    <div>
-        <asp:Panel ID="modal_div_chart" runat="server" Style="display: none" CssClass="modal-div">
-            <asp:Panel ID="modal_div_drag_chart" runat="server" Height="20px" CssClass="modal-div-drag">
-                <table style="width: 100%">
-                    <tr>
-                        <td align="center" valign="middle" style="padding-right: 5px; font-weight: bold">
-                        </td>
-                        <td style="padding-left: 5px;margin-top:-5px; width: 20px; text-align: right;">
-                            <asp:LinkButton ID="modal_cancel_button_chart" CausesValidation="false" ForeColor="Black" OnClientClick="return false;" CssClass="modal-div-cancel-button" runat="server" Text="X" ToolTip="Cancel" />
-                        </td>
-                    </tr>
-                </table>
-            </asp:Panel>
-            <asp:Panel ID="modal_div_content_chart" runat="server" CssClass="modal-div-content">
-                <asp:UpdatePanel ID="UpdatePanel11" runat="server">
-                    <ContentTemplate>
-                        <div>
-                            <highcharts:LineChart ID="hcVendas" runat="server" Width="850" Height="350" />
-                        </div>
-                        
-                        <div style="padding: 15px 10px 30px 10px; width:850px!important; text-align:center; display:table-cell; background-color:aliceblue">
-                            <asp:Label ID="lblChartTable" runat="server"></asp:Label>
-                        </div>
-                    </ContentTemplate>
-                </asp:UpdatePanel>
-            </asp:Panel>
-        </asp:Panel>
-        <asp:LinkButton ID="modal_target_button_chart" runat="server"></asp:LinkButton>
-        <asp:LinkButton ID="modal_ok_button_chart" runat="server"></asp:LinkButton>
-        <ajaxToolKit:ModalPopupExtender ID="modal_chart" PopupControlID="modal_div_chart" PopupDragHandleControlID="modal_div_drag_chart" CancelControlID="modal_cancel_button_chart" TargetControlID="modal_target_button_chart"
-            OkControlID="modal_ok_button_chart" BackgroundCssClass="ModalBackground" RepositionMode="RepositionOnWindowResizeAndScroll" DropShadow="true" Drag="true" runat="server">
-            <Animations>
-                <OnShown>
-                    <Fadein Duration="0.50" />
-                </OnShown>
-                <OnHiding>
-                    <Fadeout Duration="0.75" />
-                </OnHiding>
-            </Animations>
-        </ajaxToolKit:ModalPopupExtender>
+    <div style="float:left">
+        <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+            <ContentTemplate>
+			    <asp:ImageButton id="ImageButton1" runat="server" ImageUrl="tools/zoomfull.gif" BorderStyle="Outset" BorderWidth="1px" ToolTip="Zoom All" BorderColor="White" OnClick="zoomFull_Click"></asp:ImageButton>
+			    <aspmap:MapToolButton id="MapToolButton1" runat="server" ImageUrl="tools/zoomin.gif" Map="map" ToolTip="Zoom In"></aspmap:MapToolButton>
+			    <aspmap:MapToolButton id="MapToolButton2" runat="server" ImageUrl="tools/zoomout.gif" ToolTip="Zoom Out" Map="map" MapTool="ZoomOut"></aspmap:MapToolButton>
+			    <aspmap:MapToolButton id="MapToolButton3" runat="server" ImageUrl="tools/pan.gif" ToolTip="Pan" Map="map" MapTool="Pan"></aspmap:MapToolButton>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <strong>Background Map</strong><br />
+        <asp:RadioButton id="RadioButton1" Text="Google Maps" TextAlign="Right" AutoPostBack="True" Checked="True" OnCheckedChanged="RadioButton1_CheckedChanged" GroupName="bgmap" runat="server" /><br>
+        <asp:RadioButton id="RadioButton2" Text="OpenStreeMap" TextAlign="Right" AutoPostBack="True"  Checked="false" OnCheckedChanged="RadioButton2_CheckedChanged" GroupName="bgmap" runat="server" /><br>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <strong>Layer</strong>
+                <strong>Inudation</strong><br />
+                <asp:RadioButtonList ID="innudationList" runat="server" AutoPostBack="True">
+                </asp:RadioButtonList>
+		        <img src="WD.jpg" alt="Legend of water depth" style="height: 121px; width: 142px"/>
+                <asp:CheckBoxList ID="layerList" runat="server" AutoPostBack="True">
+                </asp:CheckBoxList>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    <div style="float:right">
+        <asp:UpdatePanel ID="UpdatePanel3" runat="server">
+            <ContentTemplate>
+                <aspmap:Map id="map" EnableSession="true" runat="server" Width="100%" 
+                        Height="700px" BackColor="#E6E6FA" ImageFormat="Gif" SmoothingMode="AntiAlias"
+					    FontQuality="ClearType" MapTool="Pan" style="margin-top: 0px"></aspmap:Map>
+			    <asp:Label ID="status" runat="server"></asp:Label>
+            </ContentTemplate>
+        </asp:UpdatePanel>
     </div>
 </asp:Content>
--- JadurChar.aspx.cs
+++ JadurChar.aspx.cs
@@ -49,306 +49,42 @@ public partial class Kulkandi : System.Web.UI.Page
         {
             App.inundation[i] = new AspMap.Layer();
         }
-            if (!IsPostBack)
-            {
-                if (map.LayerCount > 0)
-                    map.RemoveAllLayers();
-                if (map.BackgroundLayer != null)
-                    map.BackgroundLayer = null;
-                if (map.Hotspots.Count > 0)
-                    map.Hotspots.Clear();
-                if (map.MapShapes.Count > 0)
-                    map.MapShapes.Clear();
-                map.MapUnit = MeasureUnit.Meter;
-                map.ScaleBar.Visible = true;
-                map.ScaleBar.BarUnit = UnitSystem.Metric;
-                map.CoordinateSystem = new AspMap.CoordSystem(CoordSystemCode.PCS_PopularVisualisationMercator);
+        if (!IsPostBack)
+        {
+            if (map.LayerCount > 0)
+                map.RemoveAllLayers();
+            if (map.BackgroundLayer != null)
+                map.BackgroundLayer = null;
+            if (map.Hotspots.Count > 0)
+                map.Hotspots.Clear();
+            if (map.MapShapes.Count > 0)
+                map.MapShapes.Clear();
+            map.MapUnit = MeasureUnit.Meter;
+            map.ScaleBar.Visible = true;
+            map.ScaleBar.BarUnit = UnitSystem.Metric;
+            map.CoordinateSystem = new AspMap.CoordSystem(CoordSystemCode.PCS_PopularVisualisationMercator);
 
-                App.gml.MapType = GoogleMapType.Normal;
-                map.BackgroundLayer = App.gml;
-                map.ImageFormat = ImageFormat.Png;
-                map.ImageOpacity = 0.75;
+            App.gml.MapType = GoogleMapType.Normal;
+            App.gml.Visible = true;
+            map.BackgroundLayer = App.gml;
+            map.ImageFormat = ImageFormat.Png;
+            map.ImageOpacity = 0.75;
 
-                AddShapefile();
+            AddShapefile();
 
-                App.fullextent.Bottom = 2929924.996;
-                App.fullextent.Top = 2950432.713;
-                App.fullextent.Left = 9994310.118;
-                App.fullextent.Right = 10004636.304;
-                map.FullExtent = App.fullextent;
-                map.Extent = App.fullextent;
-                FillLayerList();
-                //AddOverviewMapLayers();
-            }
-        hcVendas.Exporting = new Exporting { enabled = true };
+            App.fullextent.Bottom = 2929924.996;
+            App.fullextent.Top = 2950432.713;
+            App.fullextent.Left = 9994310.118;
+            App.fullextent.Right = 10004636.304;
+            map.FullExtent = App.fullextent;
+            map.Extent = App.fullextent;
+            FillLayerList();
+        }
     }
-
-    protected void map_MarkerClick(object sender, MarkerClickEventArgs e)
-    {
-        modal_chart.Show();
-        string val = map.Markers[e.MarkerIndex].Argument;
-        string markerId = val.Substring(0, val.IndexOf("_"));
-        val = val.Substring(val.IndexOf("_") + 1, val.Length - (val.IndexOf("_") + 1));
-        string stationName = val;
-        DataTable dtStation = GetDataTable("select * from wl_station where station_name='" + stationName + "'");
-        double dangerLevel = 0.0;
-        double recordLevel = 0.0;
-        string riverName = string.Empty;
-        if (dtStation != null && dtStation.Rows.Count > 0)
-        {
-            riverName = dtStation.Rows[0]["River_Name"].ToString();
-            dangerLevel = Convert.ToDouble(dtStation.Rows[0]["Danger_Level"]);
-            recordLevel = Convert.ToDouble(dtStation.Rows[0]["RHWL"]);
-        }
-        DateTime date = DateTime.Now;
-        DateTime waterLevelDate = DateTime.Now;
-        double waterLevel = 0.0;
-        bool isFirstRow = true;
-        DataTable table = LoadCSV(stationName);
-        if (table != null)
-        {
-            if (table.Rows.Count > 0)
-            {
-                var pointObserved = new PointCollection();
-                var pointForecast = new PointCollection();
-                var pointRecorded = new PointCollection();
-                var pointDanger = new PointCollection();
-
-                foreach (DataRow dr in table.Rows)
-                {
-                    if (!string.IsNullOrEmpty(dr[1].ToString()) && !string.IsNullOrEmpty(dr[2].ToString()))
-                    {
-                        try
-                        {
-                            date = Convert.ToDateTime(dr[0]);
-                            if (isFirstRow)
-                            {
-                                pointRecorded.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), recordLevel));
-                                pointDanger.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), dangerLevel));
-                            }
-                            isFirstRow = false;
-
-                            if (!(dr[1].ToString().Equals("-9999")))
-                            {
-                                pointObserved.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), Convert.ToDouble(dr[1])));
-                                waterLevelDate = date;
-                                waterLevel = Convert.ToDouble(dr[1]);
-                            }
-                            if (!(dr[2].ToString().Equals("-9999")))
-                            {
-                                pointForecast.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), Convert.ToDouble(dr[2])));
-                            }
-
-                        }
-                        catch
-                        {
-                        }
-                    }//end
-                }//end foreach
-
-                //Exporting cd = new Exporting();
-                //cd.
-                pointRecorded.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), recordLevel));
-                pointDanger.Add(new Highcharts.Core.Point(Convert.ToInt64(date.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds), dangerLevel));
-
-                if (hcVendas.XAxis.Count > 0)
-                    hcVendas.XAxis.Clear();
-                if (hcVendas.YAxis.Count > 0)
-                    hcVendas.YAxis.Clear();
-                hcVendas.Title = new Title(stationName + " at " + riverName + " (Year " + DateTime.Now.Year.ToString() + " )");
-                hcVendas.SubTitle = new SubTitle("As on " + waterLevelDate.ToString("dd-MMM"));
-                //hcVendas.YAxis.Add(new YAxisItem { title = new Title("Water Level (mPWD)") });
-
-                ToolTip tip = new ToolTip();
-                // tooltip: {
-                //valueSuffix: '(mPWD)',
-                //shared: true,
-                //crosshairs: true,
-                //xDateFormat: '%d-%b-%Y %H:%M',
-                //valueDecimals: 2
-
-                //tip.shared.GetValueOrDefault(true);
-                //tip.shared = true;
-                tip.crosshairs = true;
-                tip.formatter = "Highcharts.dateFormat('%d-%b-%Y %H:%M', this.x) +': '+ this.y +' (mPWD)'";
-
-                hcVendas.Tooltip = tip;
-                YAxisItem y = new YAxisItem();
-                y.min = null;
-                y.title = new Title("Water Level (mPWD)");
-                y.minorGridLineWidth = 0;
-                y.gridLineWidth = 1;
-
-                PlotBands plotBands = new PlotBands();
-                PlotBand plotBand = new PlotBand();
-                plotBand.from = 0;
-                plotBand.to = 5.5;
-                plotBand.color = "#FFFFFF";
-                plotBands.Add(plotBand);
-                y.plotBands = plotBands;
-
-                hcVendas.YAxis.Add(y);
-                //YAxis y = new YAxis();
-                //y.Min = null;
-                //hcVendas.XAxis.Add(new XAxisItem { categories = new[] { "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002" } });
-                XAxisItem xAxis = new XAxisItem();
-                //xAxis.categories = xVal;
-                xAxis.type = AxisDataType.datetime;
-                //DateTimeLabelFormats dateLabelFormat =new DateTimeLabelFormats();
-                //dateLabelFormat.day="%e - %b <br /> %H:%M";
-                xAxis.dateTimeLabelFormats = new DateTimeLabelFormats { day = "%e - %b <br /> %H:%M" };
-                Labels lbl = new Labels();
-                lbl.align = Align.center;
-                lbl.step = 1;
-                xAxis.labels = lbl;
-                hcVendas.XAxis.Add(xAxis);
-                //New data collection
-
-                if (hcVendas.Series.Count > 0)
-                    hcVendas.Series.Clear();
-                var series1 = new Collection<Serie>();
-
-                if (series1.Count > 0)
-                    series1.Clear();
-                var s1 = new Serie { data = pointObserved.ToArray() };
-                s1.name = "Observed Water Level";
-                series1.Add(s1);
-
-                var s2 = new Serie { data = pointForecast.ToArray() };
-                s2.name = "Forecast Water Level";
-                series1.Add(s2);
-
-                series1.Add(new Serie { name = "Recorded Highest Water Level", data = pointRecorded.ToArray() });
-                series1.Add(new Serie { name = "Danger Water Level", data = pointDanger.ToArray() });
-                //bind 
-                //
-                PlotOptionsLine po = new PlotOptionsLine();
-                SerieStates ss = new SerieStates();
-                ss.hover = new SerieStateSettings { lineWidth = 2 };
-                po.states = ss;
-                po.lineWidth = 1;
-                po.marker = new Highcharts.Core.Marker { radius = 2 };
-                hcVendas.PlotOptions = po;
-                hcVendas.DataSource = series1;
-                hcVendas.DataBind();
-            }
-
-        }
-
-        if (dtStation != null && dtStation.Rows.Count > 0)
-        {
-            Table tbl = new Table();
-            tbl.CellPadding = 3;
-            tbl.CssClass = "info-table";
-            TableRow tr;
-            TableCell tc;
-            tr = new TableRow();
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = "River Name";
-            tc.RowSpan = 2;
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = "Station Name";
-            tc.RowSpan = 2;
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = "RHWL (mPWD)";
-            tc.RowSpan = 2;
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = "Danger Level (mPWD)";
-            tc.RowSpan = 2;
-            tr.Cells.Add(tc);
-            //tc = new TableCell();
-            //tc.CssClass = "info-table-td-head-center";
-            //tc.Text = waterLevelDate.AddDays(-1).ToString("dd-MMM");
-            //tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = waterLevelDate.ToString("dd-MMM");
-            tr.Cells.Add(tc);
-            tbl.Rows.Add(tr);
-
-            tr = new TableRow();
-            //tc = new TableCell();
-            //tc.Text = "";
-            //tc.ColumnSpan = 2;
-            //tr.Cells.Add(tc);
-            //tc = new TableCell();
-            //tc.CssClass = "info-table-td-head-center";
-            //tc.Text = waterLevelDate.AddDays(-1).ToString("hh:mm tt");
-            //tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-head-center";
-            tc.Text = waterLevelDate.ToString("hh:mm tt");
-            tr.Cells.Add(tc);
-            tbl.Rows.Add(tr);
-
-            tr = new TableRow();
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-center";
-            tc.Text = riverName;
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-center";
-            tc.Text = stationName;
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-center";
-            tc.Text = recordLevel.ToString();
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-center";
-            tc.Text = dangerLevel.ToString();
-            tr.Cells.Add(tc);
-            tc = new TableCell();
-            tc.CssClass = "info-table-td-center";
-            tc.Text = waterLevel.ToString();
-            tr.Cells.Add(tc);
-            //tc = new TableCell();
-            //tc.CssClass = "info-table-td-center";
-            //tc.Text = dangerLevel.ToString();
-            //tr.Cells.Add(tc);
-            tbl.Rows.Add(tr);
-
-            StringWriter sw = new StringWriter();
-            tbl.RenderControl(new HtmlTextWriter(sw));
-            lblChartTable.Text = sw.ToString();
-            //riverName = dtStation.Rows[0]["River_Name"].ToString();
-            //dangerLevel = Convert.ToDouble(dtStation.Rows[0]["Danger_Level"]);
-            //recordLevel = Convert.ToDouble(dtStation.Rows[0]["RHWL"]);
-        }
-        modal_chart.Show();
-    }
-
 
     protected void Page_PreRender(object sender, System.EventArgs e)
     {
-        //UpdateLayerProperties();
         UpdateLayerVisibility();
-        //UpdateOverviewMapProperties();
-
-        // set legend properties
-        legend.AutoSize = true;
-        legend.Add("Legend:"); // title
-        legend.LegendFont.Name = "Arial";
-        legend.LegendFont.Size = 16;
-        legend.LegendFont.Bold = true;
-
-        // populate the legend from the map layers collection
-        // see also the Legend.Populate method
-        for (int i = 0; i < map.LayerCount; i++)
-        {
-            if (map.IsLayerVisible(i))
-            {
-                AspMap.Layer layer = map[i];
-                legend.Add(layer.Name, layer.LayerType, layer.Symbol);
-            }
-        }
     }
 
     private void FillLayerList()
@@ -377,7 +113,6 @@ public partial class Kulkandi : System.Web.UI.Page
                 map[item.Value].Visible = item.Selected;
             }
         }
-
     }
 
     protected void zoomFull_Click(object sender, System.Web.UI.ImageClickEventArgs e)
@@ -588,126 +323,15 @@ public partial class Kulkandi : System.Web.UI.Page
 
     }
 
-    public DataTable LoadCSV(string fileName)
-    {
-        DataTable dt = new DataTable("csv");
-        try
-        {
-            // Creates and opens an ODBC connection
-            string strConnString = "Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + Server.MapPath("Model_data") + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\";Persist Security Info=False";
-
-            OleDbConnection conn = new OleDbConnection(strConnString.Trim());
-            string sql = string.Empty;
-
-
-            sql = "select * from [" + fileName + ".csv]";
-            conn.Open();
-            //Creates the data adapter
-            OleDbDataAdapter da = new OleDbDataAdapter(sql, conn);
-
-            //Fills dataset with the records from CSV file
-            da.Fill(dt);
-            da.Dispose();
-            //closes the connection
-            conn.Close();
-            conn.Dispose();
-        }
-        catch (Exception e) //Error
-        {
-        }
-        return dt;
-    }
-    public void AddChart(string fileName)
-    {
-
-
-        //LoadCSV(100);
-
-        //string strFileName = "D://FXDEAL.csv";
-        //OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + System.IO.Path.GetDirectoryName(strFileName) + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\"");
-        OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + Server.MapPath("Model_data") + "; Extended Properties = \"Text;HDR=YES;FMT=Delimited\"");
-
-        conn.Open();
-        OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Aricha.csv]", conn);
-        DataSet ds = new DataSet("Temp");
-        adapter.Fill(ds);
-        DataTable tb = ds.Tables[0];
-
-        string cd = Server.MapPath("Model_data") + "\\Aricha.csv";
-        //string conStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("Model_data") + ";Extended Properties='text;HDR=Yes;IMEX=1;FMT=Delimited(,)';";
-        //OleDbDataAdapter da = new OleDbDataAdapter("select * from Aricha", conStr);
-        //DataTable dt = new DataTable();
-        //try
-        //{
-        //    da.Fill(dt);
-        //}
-        //catch (Exception ex)
-        //{
-        //}
-
-    }
-
-    public DataTable GetDataTable(string sql)
-    {
-        OleDbDataAdapter da = new OleDbDataAdapter(sql, "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\ffwc.mdb;");
-        DataTable dt = new DataTable();
-        try
-        {
-            da.Fill(dt);
-        }
-        catch
-        {
-            dt = null;
-        }
-        finally
-        {
-            da.Dispose();
-        }
-        return dt;
-    }
-
-    public string GetFieldValue(string sql)
-    {
-        string retVal = string.Empty;
-        OleDbConnection cnn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\ffwc.mdb;");
-
-        OleDbCommand cmd = new OleDbCommand();
-        try
-        {
-            cnn.Open();
-            cmd.Connection = cnn;
-            cmd.CommandText = sql;
-            retVal = cmd.ExecuteScalar().ToString();
-
-        }
-        catch
-        {
-            retVal = string.Empty;
-        }
-        finally
-        {
-            cmd.Dispose();
-            cnn.Close();
-            cnn.Dispose();
-        }
-
-
-        return retVal;
-    }
-
     protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
     {
         if (RadioButton1.Checked)
         {
             App.extent = map.Extent;
             App.gml.MapType = GoogleMapType.Normal;
+            App.gml.Visible = true;
+            App.osmLayer.Visible = false;
             map.BackgroundLayer = App.gml;
-            map.Extent = App.extent;
-        }
-        else
-        {
-            App.extent = map.Extent;
-            map.BackgroundLayer = App.osmLayer;
             map.Extent = App.extent;
         }
     }
@@ -718,13 +342,8 @@ public partial class Kulkandi : System.Web.UI.Page
         {
             App.extent = map.Extent;
             map.BackgroundLayer = App.osmLayer;
-            map.Extent = App.extent;
-        }
-        else
-        {
-            App.extent = map.Extent;
-            App.gml.MapType = GoogleMapType.Normal;
-            map.BackgroundLayer = App.gml;
+            App.gml.Visible = false;
+            App.osmLayer.Visible = true;
             map.Extent = App.extent;
         }
     }
